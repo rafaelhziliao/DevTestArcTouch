3d2c360f68eb4e41f1aebd784773632b31e3ae6f
Rafael Ziliao <rafaelh.ziliao@gmail.com>
2018-08-05 01:27:53 -0300

    Fix Movie Model

--- DevTestArcTouch/Models/Movie.swift
+++ DevTestArcTouch/Models/Movie.swift
@@ -11,24 +11,22 @@ import Foundation
 struct Movie {
     let id: Int
     let posterPath: String
-    let videoPath: String
     let backDrop: String
     let title: String
-    let releaseDate: String
-    let rating: String
+    var releaseDate: String
     let overview: String
+    let voteAverage: Double
 }
 
 extension Movie: Decodable {
     enum MovieCodingKeys: String, CodingKey {
         case id
         case posterPath = "poster_path"
-        case videoPath
         case backDrop = "backdrop_path"
         case title
         case releaseDate = "release_date"
-        case rating = "vote_avaerage"
         case overview
+        case voteAverage = "vote_average"
     }
     
     init(from decoder: Decoder) throws {
@@ -36,12 +34,10 @@ extension Movie: Decodable {
         
         id = try container.decode(Int.self, forKey: .id)
         posterPath = try container.decode(String.self, forKey: .posterPath)
-        videoPath = try container.decode(String.self, forKey: .videoPath)
         backDrop = try container.decode(String.self, forKey: .backDrop)
         title = try container.decode(String.self, forKey: .title)
         releaseDate = try container.decode(String.self, forKey: .releaseDate)
-        rating = try container.decode(String.self, forKey: .rating)
         overview = try container.decode(String.self, forKey: .overview)
-
+        voteAverage = try container.decode(Double.self, forKey: .voteAverage)
     }
 }
