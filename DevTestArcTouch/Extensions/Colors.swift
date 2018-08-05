5a9862fe1ebd867cd4c9e9eb3d27129e4ecb39dd
Rafael Ziliao <rafaelh.ziliao@gmail.com>
2018-08-05 01:20:05 -0300

    Add helper extensin for colors

---
+++ DevTestArcTouch/Extensions/Colors.swift
@@ -0,0 +1,24 @@
+//
+//  Colors.swift
+//  DevTestArcTouch
+//
+//  Created by Rafael Zilião on 03/08/2018.
+//  Copyright © 2018 Rafael Zilião. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    struct Colors {
+        static let primaryColor = UIColor(red:0.95, green:0.61, blue:0.07, alpha:1.0)
+        static let lightColor = UIColor(red:1, green:1, blue:1, alpha:0.84)
+        static let grayColor = UIColor(red:1, green:1, blue:1, alpha:0.60)
+        static let dangerColor = UIColor(red:0.75, green:0.22, blue:0.17, alpha:1.0)
+        static let lowRatingBackgroundColor = UIColor(red:0.15, green:0.68, blue:0.38, alpha:1.0)
+        static let highRatingBackgroundColor = UIColor(red:0.95, green:0.77, blue:0.06, alpha:1.0)
+        static let primaryBackgroundColor = UIColor(red:0.11, green:0.12, blue:0.09, alpha:1.0)
+        static let highlightBackgroundColor = UIColor(red:0.15, green:0.16, blue:0.13, alpha:1.0)
+        static let borderColor = UIColor(red:0.24, green:0.24, blue:0.24, alpha:1.0)
+    }
+}
