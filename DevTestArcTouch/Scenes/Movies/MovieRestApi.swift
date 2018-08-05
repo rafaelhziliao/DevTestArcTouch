74033cd04254477f40d6cea833aab820b2451293
Rafael Ziliao <rafaelh.ziliao@gmail.com>
2018-08-05 01:25:52 -0300

    Implement MovieService

---
+++ DevTestArcTouch/Scenes/Movies/MovieRestApi.swift
@@ -0,0 +1,25 @@
+//
+//  MovieRestApi.swift
+//  DevTestArcTouch
+//
+//  Created by Rafael Zilião on 04/08/2018.
+//  Copyright © 2018 Rafael Zilião. All rights reserved.
+//
+
+import Foundation
+
+class MovieRestApi: MovieService {
+    
+    func requestMovies(page: Int,
+                       success: @escaping ([Movie]) -> Void,
+                       failure: @escaping (Error) -> Void = {_ in } ) {
+        
+        MovieResults.requestNewMovies(page: page, success: { (movies) in
+            success(movies)
+        }) { (error) in
+            failure (error)
+            
+        }
+    }
+    
+}
